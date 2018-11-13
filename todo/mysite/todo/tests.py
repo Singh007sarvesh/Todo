5e84a330f952659a1be927dda56c53f7639c9e24
singh007sarvesh <007sarveshsingh@gmail.com>
2018-11-13 08:12:57 +0530

    change in url.py

--- todo/mysite/todo/tests.py
+++ todo/mysite/todo/tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 from todo.models import Todo
 from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
-# Create your tests here.
 
-class TodoAPITest(TestCase):
+
+class TodoListAPITest(TestCase):
 	def setUp(self):
-		todo = [
+		self.todo = [
             {
             	"title": "anurag",
                 "description": "anurag@nitc",
@@ -28,5 +27,40 @@ class TodoAPITest(TestCase):
 	
 	def test_todolist(self):
 		client = APIClient()
-		response = client.get('/todos/',{'title': 'anurag', 'description': 'anurag@nitc', 'due_date': '11/05/2018'})
+		response = client.get('/todos/')
 		self.assertEqual(response.status_code, 200)
+		todo_title = 'Diwali'
+		desc = 'abc'
+		date = '11/13/2018'
+		data = Todo(title = todo_title, description = desc, due_date = date)
+		response = client.post('/todos/', {'title':todo_title,'description': desc,'due_date':date})
+		self.assertEquals(response.status_code,201)
+
+
+# class TodoSearchAPITest(TestCase):
+
+# 	def setUp(self):
+# 		self.todo = [
+#             {
+#             	"title": "anurag",
+#                 "description": "anurag@nitc",
+#                 "due_date": "11/05/2018"
+#             },
+#             {
+#                 "title": "anurag1",
+#                 "description": "anurag@nitc1",
+#                 "due_date": "11/25/2018"
+#             },
+#         ]
+
+# 	def test_todo_search(self):
+# 		client = APIClient()
+# 		import pdb;pdb.set_trace()
+# 		todo_list = Todo.objects.filter(title = "Diwali")
+# 		if todo_list:
+# 			print("hello---->")
+# 			response = client.get('/todos/?/')
+# 			self.assertEquals(response.status_code,200)
+# 		# if todo_search:
+# 		# 	return render(
+# 		# 	request, 'todo/home.html', {'todo_search_list': todo_list})
--- todo/mysite/todo/urls.py
+++ todo/mysite/todo/urls.py
@@ -3,7 +3,7 @@ from todo import views
 from django.urls import path
 urlpatterns = [
 	# path('', views.TodoList.as_view()),
-	path('?', views.TodoSearch.as_view(), name='search'),
-	path('', views.TodoList.as_view(), name='addTodo'),
+	path('todos/?', views.TodoSearch.as_view(), name='search'),
+	path('todos/', views.TodoList.as_view(), name='addTodo'),
 	
 ]
